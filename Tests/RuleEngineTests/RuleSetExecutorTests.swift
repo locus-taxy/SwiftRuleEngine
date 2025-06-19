5f81a00631609d2fcc468a9ded1d995afe768078
kanj-locus <kanj@locus.sh>
2025-06-19 12:15:41 +0530

    Test sample with anyOf matching

--- Tests/RuleEngineTests/RuleSetExecutorTests.swift
+++ Tests/RuleEngineTests/RuleSetExecutorTests.swift
@@ -34,13 +34,86 @@ struct RuleSetExecutorTests {
         let ruleSet: RuleSet = try loadJSON(filename: "sample-rule-set-2")
         let executor = RuleSetExecutorImpl<TestResult>()
 
-        let testTour = TestExtendedTour(status: TestExtendedTourStatus(status: .queued))
+        let testTour1 = TestExtendedTour(status: TestExtendedTourStatus(status: .queued))
         let result1 = try executor.executeFirstMatch(
             clientId: "clientId", ruleSet: ruleSet,
-            consumerContext: TestConsumerContext(tour: testTour),
+            consumerContext: TestConsumerContext(tour: testTour1),
             timeZoneProvider: TestTimeZoneProvider()
         )
         #expect(result1 == .proceed)
+
+        let testTour2 = TestExtendedTour(status: TestExtendedTourStatus(status: .started))
+        let result2 = try executor.executeFirstMatch(
+            clientId: "clientId", ruleSet: ruleSet,
+            consumerContext: TestConsumerContext(tour: testTour2),
+            timeZoneProvider: TestTimeZoneProvider()
+        )
+        #expect(result2 == nil)
+
+        let testTour3 = TestExtendedTour(status: TestExtendedTourStatus(status: nil))
+        let result3 = try executor.executeFirstMatch(
+            clientId: "clientId", ruleSet: ruleSet,
+            consumerContext: TestConsumerContext(tour: testTour3),
+            timeZoneProvider: TestTimeZoneProvider()
+        )
+        #expect(result3 == nil)
+
+        let testTour4 = TestExtendedTour(status: nil)
+        #expect(throws: RuleEngineError.self, performing: {
+            try executor.executeFirstMatch(
+                clientId: "clientId", ruleSet: ruleSet,
+                consumerContext: TestConsumerContext(tour: testTour4),
+                timeZoneProvider: TestTimeZoneProvider()
+            )
+        })
+
+        #expect(throws: RuleEngineError.self, performing: {
+            try executor.executeFirstMatch(
+                clientId: "clientId", ruleSet: ruleSet,
+                consumerContext: TestConsumerContext(),
+                timeZoneProvider: TestTimeZoneProvider()
+            )
+        })
+    }
+
+    @Test func testSample3() async throws {
+        let ruleSet: RuleSet = try loadJSON(filename: "sample-rule-set-3")
+        let executor = RuleSetExecutorImpl<TestResult>()
+
+        let result1 = try executor.executeFirstMatch(
+            clientId: "clientId", ruleSet: ruleSet,
+            consumerContext: TestConsumerContext(visitStatus: .started),
+            timeZoneProvider: TestTimeZoneProvider()
+        )
+        #expect(result1 == .proceed)
+
+        let result2 = try executor.executeFirstMatch(
+            clientId: "clientId", ruleSet: ruleSet,
+            consumerContext: TestConsumerContext(visitStatus: .arrived),
+            timeZoneProvider: TestTimeZoneProvider()
+        )
+        #expect(result2 == .proceed)
+
+        let result3 = try executor.executeFirstMatch(
+            clientId: "clientId", ruleSet: ruleSet,
+            consumerContext: TestConsumerContext(visitStatus: .transacting),
+            timeZoneProvider: TestTimeZoneProvider()
+        )
+        #expect(result3 == .proceed)
+
+        let result4 = try executor.executeFirstMatch(
+            clientId: "clientId", ruleSet: ruleSet,
+            consumerContext: TestConsumerContext(visitStatus: .completed),
+            timeZoneProvider: TestTimeZoneProvider()
+        )
+        #expect(result4 == .proceedWithConfirmation)
+
+        let result5 = try executor.executeFirstMatch(
+            clientId: "clientId", ruleSet: ruleSet,
+            consumerContext: TestConsumerContext(visitStatus: .accepted),
+            timeZoneProvider: TestTimeZoneProvider()
+        )
+        #expect(result5 == nil)
     }
 }
 
@@ -48,10 +121,12 @@ struct TestConsumerContext: ConsumerContext {
 
     private let location: TestLocation?
     private let tour: TestExtendedTour?
+    private let visitStatus: TestVisitStatus?
 
-    init(location: TestLocation? = nil, tour: TestExtendedTour? = nil) {
+    init(location: TestLocation? = nil, tour: TestExtendedTour? = nil, visitStatus: TestVisitStatus? = nil) {
         self.location = location
         self.tour = tour
+        self.visitStatus = visitStatus
     }
 
     var inputParams: [String: Any] {
@@ -64,6 +139,10 @@ struct TestConsumerContext: ConsumerContext {
 
         if let extendedTour = self.tour {
             inputs["extendedTour"] = extendedTour
+        }
+
+        if let nextVisitStatus = self.visitStatus {
+            inputs["nextVisitStatus"] = nextVisitStatus
         }
 
         return inputs
@@ -89,9 +168,21 @@ enum TestTourStatus: String, Codable {
     case completed = "COMPLETED"
 }
 
+enum TestVisitStatus: String, Codable {
+    case received = "RECEIVED"
+    case waiting = "WAITING"
+    case accepted = "ACCEPTED"
+    case started = "STARTED"
+    case arrived = "ARRIVED"
+    case transacting = "TRANSACTING"
+    case completed = "COMPLETED"
+    case cancelled = "CANCELLED"
+}
+
 enum TestResult: String, Codable {
     case blockWithReasonUnknown = "BLOCK_WITH_REASON_UNKNOWN"
     case proceed = "PROCEED"
+    case proceedWithConfirmation = "PROCEED_WITH_CONFIRMATION"
 }
 
 struct TestTimeZoneProvider: TimeZoneProvider {
